e22d1d8d41ff1ba578430147324de54763adb5e7
Fábio Matias <fabio_mcm2@hotmail.com>
2021-10-27 16:31:33 +0100

    chore: stop minting NFTs on deploy

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,9 +4,9 @@ const main = async () => {
   await nftContract.deployed();
   console.log("Contract deployed to:", nftContract.address);
 
-  let txn = await nftContract.makeAnEpicNFT();
-  await txn.wait();
-  console.log("Minted NFT #1");
+  // let txn = await nftContract.makeAnEpicNFT();
+  // await txn.wait();
+  // console.log("Minted NFT #1");
 };
 
 const runMain = async () => {
